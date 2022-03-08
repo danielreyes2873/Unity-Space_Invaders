5dec7ebd136db3e68e9a1879b76e8667ecfafed4
Daniel Reyes <daniereyes@csumb.edu>
2022-03-08 01:05:34 -0800

    Implemented persistent high score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,22 +9,39 @@ public class ScoreManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highScoreText;
     private int score = 0;
+    private int highScore = 0;
+    private string highScoreKey = "HighScore";
     private string scoreFormat = "0000.##";
     // Start is called before the first frame update
     void Start()
     {
-        
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        highScoreText.text = highScore.ToString(scoreFormat);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            //reset high score
+            PlayerPrefs.SetInt(highScoreKey,0);
+            PlayerPrefs.Save();
+        }
     }
 
     public void AddPoints(int points)
     {
         score += points;
         scoreText.text = score.ToString(scoreFormat);
+    }
+
+    private void OnDisable()
+    {
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt(highScoreKey,score);
+            PlayerPrefs.Save();
+        }
     }
 }
